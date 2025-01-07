ea9cd9d3e4e3dea79a69f6a7aaf4f0f748940bfc
GABCARL <gabriele.carlone@marconibari.edu.it>
2025-01-07 17:35:24 +0000

    consegnato

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,37 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+public static void main(String args[])
+{
+   Scanner in = new Scanner(System.in);
+    
+   int n;
+   int duplicati;
 
-//Import di Classi Java necessarie al funzionamento del programma
-import java.util.Scanner;
+   System.out.print("Inserire la granzedda del vettore: ");
+   n = in.nextInt();
+   int v[] = new int[n];
+   for(int i = 0; i < n; i++){
+    System.out.print("Inserire il " + (i+1) + " numero: ");
+    v[i] = in.nextInt();
+   }
+   
+   boolean visitato[] = new boolean[n];
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
-
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+   for(int i = 0; i < n; i++){
+        if(!visitato[i]){
+            duplicati = 1;
+            for(int j = i+1; j < n; j++){
+                if(v[i] == v[j]){
+                    duplicati++;
+                    visitato[j] = true;
+                    System.out.println("Il valore " + v[i] + " occorre nelle posizioni " + i + "," + j );
+                }
+            }
+            if(duplicati > 1){
+                System.out.println("E' stato inserito " + duplicati + " volte");
+            }
+        }
     }
+
 }
+
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
